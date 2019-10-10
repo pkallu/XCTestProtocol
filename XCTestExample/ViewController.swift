f1c7b881a40a230037f80d58d78239b095a3020f
Pragathi Kallu <p.kallu@outlook.com>
2019-10-10 07:58:45 -0500

    Added all the files

--- XCTestExample/ViewController.swift
+++ XCTestExample/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonShare: UIBarButtonItem!
+    @IBOutlet weak var imageToShare: UIImageView!
+    @IBOutlet weak var segmentedControlColor: UISegmentedControl!
+    
+    @IBAction func shareTapped(_ sender: Any) {
+        if let image = imageToShare.image {
+        var imageToShare = image
+            if segmentedControlColor.selectedSegmentIndex == 1 {
+                guard let currentCGImage = image.cgImage else { return }
+                let currentCIImage = CIImage(cgImage: currentCGImage)
+
+                let filter = CIFilter(name: "CIColorMonochrome")
+                filter?.setValue(currentCIImage, forKey: "inputImage")
+
+                           // set a gray value for the tint color
+                filter?.setValue(CIColor(red: 0.7, green: 0.7, blue: 0.7), forKey: "inputColor")
+
+                filter?.setValue(1.0, forKey: "inputIntensity")
+                guard let outputImage = filter?.outputImage else { return }
+
+                let context = CIContext()
+
+                if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                    let processedImage = UIImage(cgImage: cgimg)
+                    imageToShare = processedImage
+                }
+            }
+           
+            let vc = UIActivityViewController(activityItems: [imageToShare], applicationActivities: [])
+            present(vc, animated: true)
+        }
+    }
+    //MARK -- Life cycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
